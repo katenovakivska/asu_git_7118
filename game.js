9edcb63032088b7fe2b298ceedaae57661df5fba
Kate Novakivska <kate.nova14@gmail.com>
2019-04-04 16:19:35 +0300

    Form of asteroid was changed!

--- game.js
+++ game.js
@@ -635,16 +635,9 @@ AlienBullet.prototype = new Bullet();
 
 Asteroid = function () {
   this.init("asteroid",
-            [-10,   0,
-              -5,   7,
-              -3,   4,
-               1,  10,
-               5,   4,
-              10,   0,
-               5,  -6,
-               2, -10,
-              -4, -10,
-              -4,  -5]);
+            [-1,   1,
+              1,   -1,
+              1,   1]);
 
   this.visible = true;
   this.scale = 6;
@@ -1212,4 +1205,4 @@ $(function () {
   });
 });
 
-// vim: fdl=0
+
